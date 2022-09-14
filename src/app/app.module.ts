1a43e840184fb2bb0a231ccfb792839abc7149fc
Jazher <pragmaticube@protonmail.com>
2022-09-14 09:33:59 -0300

    Create new branch on work to add components of [anexo A]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaAlumnoComponent } from './componentes/lista-alumno/lista-alumno.component';
+import { FormularioSeccionComponent } from './componentes/formulario-seccion/formulario-seccion.component';
+import { FormularioAlumnoComponent } from './componentes/formulario-alumno/formulario-alumno.component';
+import { ListaHistorialComponent } from './componentes/lista-historial/lista-historial.component';
+import { HistorialComponent } from './componentes/historial/historial.component';
+import { AlumnoComponent } from './componentes/alumno/alumno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaAlumnoComponent,
+    FormularioSeccionComponent,
+    FormularioAlumnoComponent,
+    ListaHistorialComponent,
+    HistorialComponent,
+    AlumnoComponent
   ],
   imports: [
     BrowserModule
